cfd523e220f48aa2688ed7fc827b0cc364bd07fd
SafronovaLiza <mava3328@gmail.com>
2024-07-03 12:54:52 +0400

    Работающий код для класса SimpleDotCom и тестовый код для этого же класса (тесты пройдены)

--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -1,16 +1,29 @@
 public class SimpleDotCom {
-    private int[] locationSells;
-    private int numOfHints;
+    private int[] locationCells; //расположение сайта
+    private int numOfHints; //количество попаданий
 
     void setLocationSells(int[] locations){
-        //Задаем расположение сайта
-        //три последовательные ячейки в массиве
+        locationCells = locations;
     }
 
-    String checkYourself(String guess){
+    String checkYourself(String stringGuess){
         //Проверяем, попал ли пользователь
         //Метод принимает попытку пользователя (1, 2 и тд), проверяет есть ли на выбранной ячейке сайт
         // и возвращает ответ "мимо", "попал", "убил" в зависимости от успешности попытки
-        return "";
+
+        int guess = Integer.parseInt(stringGuess);
+        String result = "Мимо";
+
+        for (int cell : locationCells){
+            if (guess == cell){
+                numOfHints++;
+                if (numOfHints == locationCells.length)
+                    result = "Потопил";
+                else
+                    result = "Попал";
+                return result;
+            }
+        }
+        return result;
     }
 }
